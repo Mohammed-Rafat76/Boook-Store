cfdb404214229e221e68012cbf8d9d4aa1fbd5db
Mohammed-Rafat76 <mmed600811@gmail.com>
2026-02-01 15:45:45 +0200

    add class for out let

--- src/mainLayOut.jsx
+++ src/mainLayOut.jsx
@@ -4,7 +4,7 @@ import Footer from "./Component/footer";
 
 export default function MainLayOut() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Header />
       <Outlet />
       <Footer />
