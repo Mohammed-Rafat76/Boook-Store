07cb585c09fedacb735c67fff730414e49ef1ed0
Mohammed-Rafat76 <mmed600811@gmail.com>
2026-02-10 20:32:30 +0200

    edit for responsive of microfon icon in searsh input

--- src/Component/searshInput.jsx
+++ src/Component/searshInput.jsx
@@ -9,7 +9,7 @@ export default function SearshInput() {
           <div className="w-full h-full">
             <label className="input p-0 m-0 bg-[#FFFFFF] rounded-l-[50px] xl:w-100 lg:w-75 md:w-50 w-25 h-full validator text-violet-700 join-item">
               <input type="text" placeholder="Search" className="placeholder:text-[9px] md:placeholder:text-[15px] placeholder:pl-2   placeholder:text-[#22222280]" required />
-              <IoMicOutline className="w-4 h-4 text-[#22222280]"/>
+              <IoMicOutline className="w-4 h-4 lg:w-5 lg:h-5 xl:w-6 xl:h-6 text-[#22222280]"/>
             </label>
           </div>
           <button className=" px-4 bg-[#D9176C] text-center h-full rounded-r-[50px]"><FaSearch className="w-2 md:w-3 h-2 md:h-4 lg:w-5 lg:h-5 xl:w-5 xl:h-5" /></button>
